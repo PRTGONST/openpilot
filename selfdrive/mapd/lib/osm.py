d253cfa91b33453f4a14cd2d4e4b155a58c7bab0
alfhern <hz.alfonso@gmail.com>
2021-05-12 12:59:10 +0200

    Simulator: Provide GPS messages with Carla GNSS values and load OSM data from XML file

--- selfdrive/mapd/lib/osm.py
+++ selfdrive/mapd/lib/osm.py
@@ -1,4 +1,15 @@
 import overpy
+import os
+
+
+_SIM = "SIMULATION" in os.environ
+_DEBUG = True
+
+
+def _debug(msg):
+  if not _DEBUG:
+    return
+  print(msg)
 
 
 class OSM():
@@ -7,19 +18,34 @@ class OSM():
     self.api = overpy.Overpass(url='http://3.65.170.21/api/interpreter')
 
   def fetch_road_ways_around_location(self, lat, lon, radius):
-      # fetch all ways and nodes on this ways around location
-      around_str = f'{str(radius)},{str(lat)},{str(lon)}'
-      q = """
-          way(around:""" + around_str + """)
-            [highway]
-            [highway!~"^(footway|path|bridleway|steps|cycleway|construction|bus_guideway|escape|service)$"];
-          (._;>;);
-          out;
-          """
-      try:
-        ways = self.api.query(q).ways
-      except Exception as e:
-        print(f'Exception while querying OSM:\n{e}')
-        ways = []
+    if _SIM:
+      return self.simulator_fetch()
 
-      return ways
+    # fetch all ways and nodes on this ways around location
+    around_str = f'{str(radius)},{str(lat)},{str(lon)}'
+    q = """
+        way(around:""" + around_str + """)
+          [highway]
+          [highway!~"^(footway|path|bridleway|steps|cycleway|construction|bus_guideway|escape|service)$"];
+        (._;>;);
+        out;
+        """
+    try:
+      ways = self.api.query(q).ways
+    except Exception as e:
+      print(f'Exception while querying OSM:\n{e}')
+      ways = []
+
+    return ways
+
+  def simulator_fetch(self):
+    _debug('OSM: Start simulator fetch')
+
+    path = os.getenv("CARLA_OVERPASS_XML_PATH", "../mapd/lib/carla_town04_osm_overpass.xml")
+
+    with open(path, 'r') as f:
+      overpass_xml = f.read()
+      ways = self.api.parse_xml(overpass_xml).ways
+
+    _debug('OSM: Finish simulator fetch')
+    return ways
