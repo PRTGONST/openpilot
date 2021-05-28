585302efdbd66467ee5e140ef926b510c9636b53
alfhern <hz.alfonso@gmail.com>
2021-05-28 12:06:09 +0200

    UI: Add Debug UI toggle to control visibility of debug UI elements - start with turn vision ui

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -194,6 +194,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"RecordFrontLock", PERSISTENT},  // for the internal fleet
     {"ReleaseNotes", PERSISTENT},
     {"ShouldDoUpdate", CLEAR_ON_MANAGER_START},
+    {"ShowDebugUI", PERSISTENT},
     {"SpeedLimitControl", PERSISTENT},
     {"SpeedLimitPercOffset", PERSISTENT},
     {"SubscriberInfo", PERSISTENT},
